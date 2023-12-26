ae78da1da167ad55e45fdaaf9c14c3f165a99934
DHARMENDRA PRAJAPATI <dharmendraprajapati9503@gmail.com>
2023-12-26 14:15:39 +0530

    ApiError and ApiResponse util created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-dotenv.config({path: './env'});
+dotenv.config({ path: './env' });
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server running on port ${process.env.PORT || 8000}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failes !!! ", err);
+    });
