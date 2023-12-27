5050d9c8a8f48a3c676eae6325de9999a78de53f
DHARMENDRA PRAJAPATI <dharmendraprajapati9503@gmail.com>
2023-12-27 12:52:20 +0530

    Added user and video model

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect
         (`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
-        console.log(`\n MongoDB connected !! DB: ${connectionInstance.connection.host}:${connectionInstance.connection.port}`);
+        console.log(`\nMongoDB connected !! DB: ${connectionInstance.connection.host}:${connectionInstance.connection.port}`);
     } catch (error) {
         console.log("MONGODB connection FAILED: ", error);
         process.exit(1);
