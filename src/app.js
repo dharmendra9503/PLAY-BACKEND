75b75565f7b25943bcb96336b2b5a91d2f306719
DHARMENDRA PRAJAPATI <dharmendraprajapati9503@gmail.com>
2024-02-05 23:03:26 +0530

    Project setup done with register route

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.use(express.static("public"));
 app.use(cookieParser()); 
 
 
+//routes import
+import userRouter from './routes/user.routes.js';
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
 export default app;
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({
+        message: "Welcome to PLAY backend"
+    })
+})
+
+export {
+    registerUser,
+}
