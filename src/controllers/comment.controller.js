5b1c3d36813d7d1b65862008c1721e1350de3747
DHARMENDRA PRAJAPATI <dharmendraprajapati9503@gmail.com>
2024-04-05 17:27:30 +0530

    Add video comments functionality

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,21 +1,128 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Comment } from "../models/comment.model.js";
+import { findVideoById } from "../services/video.service.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
+import { getMongoosePaginationOptions } from "../utils/helpers.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
-    const { videoId } = req.params
-    const { page = 1, limit = 10 } = req.query
+    const { videoId } = req.params;
+    const { page, limit } = req.query;
+
+    const video = await findVideoById(videoId);
+    if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+
+    const commentAggregate = Comment.aggregate([
+        {
+            $match: { video: new mongoose.Types.ObjectId(videoId) }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner"
+            }
+        },
+        {
+            $unwind: "$owner"
+        },
+        {
+            $project: {
+                __v: 0, // Exclude the _id field
+                videoFile: 0,
+                playbackURL: 0,
+                "owner.password": 0,
+                "owner.__v": 0,
+                "owner.updatedAt": 0,
+                "owner.createdAt": 0,
+                "owner.email": 0,
+                "owner.fullName": 0,
+                "owner.coverImage": 0,
+                "owner.watchHistory": 0,
+                "owner.refreshToken": 0,
+            }
+        },
+        {
+            $sort: { createdAt: -1 }
+        }
+    ]);
+
+    const comments = await Comment.aggregatePaginate(
+        commentAggregate,
+        getMongoosePaginationOptions({
+            page,
+            limit,
+            customLabels: {
+                totalDocs: "totalComments",
+                docs: "comments",
+            },
+        })
+    );
+
+    // TODO: Add like count
+
+    return res.status(200).json(new ApiResponse(200, comments, "Comments fetched successfully"));
 })
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
+    const { content } = req.body;
+    const { videoId } = req.params;
+
+    const video = await findVideoById(videoId);
+    if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+
+    const commentDetails = {
+        content,
+        video: video._id,
+        owner: req.user?._id
+    }
+    const createdComment = await Comment.create(commentDetails);
+    if (!createdComment) {
+        throw new ApiError(400, "Failed to create comment");
+    }
+
+    return res.status(201).json(new ApiResponse(201, createdComment, "Comment created successfully"));
 })
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
+    const { commentId } = req.params;
+    const { content } = req.body;
+
+    const comment = await Comment.findById(commentId).select("-__v");
+    if (!comment) {
+        throw new ApiError(404, "Comment not found");
+    }
+    if (comment.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(403, "You are not authorized to update this comment");
+    }
+
+    comment.content = content;
+    await comment.save({ validateBeforeSave: false });
+
+    return res.status(200).json(new ApiResponse(200, comment, "Comment updated successfully"));
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
+    const { commentId } = req.params;
+
+    const existedComment = await Comment.findById(commentId);
+    if (!existedComment) {
+        throw new ApiError(404, "Comment not found");
+    }
+    if (existedComment.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(403, "You are not authorized to delete this comment");
+    }
+
+    await Comment.deleteOne(new mongoose.Types.ObjectId(commentId));
+
+    return res.status(200).json(new ApiResponse(200, {}, "Comment deleted successfully"));
 })
 
 export {
